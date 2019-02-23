f3e511e49657c23e40ca1dbce78a257166eec308
3LOGIXS\dhanushka_b <dhanushka.bandara@outlook.com>
2019-02-23 13:45:05 +0530

    get post list

---
+++ React/src/containers/PostsList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Post from "../components/Post";
+import axios from "axios";
+const listData = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: "http://ant.design",
+    title: `ant design part ${i}`,
+    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+    description:
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+    content:
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently."
+  });
+}
+class PostList extends React.Component {
+  state = {
+    post: []
+  };
+  componentDidMount() {
+    axios.get("https://api.devrant.thusitha.site/v1/post.list").then(res => {
+      this.setState({
+        post: res.data
+      });
+      console.log(res.data);
+    });
+  }
+  render() {
+    return <Post data={listData} />;
+  }
+}
+
+export default PostList;
--- React/src/index.js
+++ React/src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "./styles.css";
+import "antd/dist/antd.css";
+import CustomLayout from "./containers/Layout";
+import PostList from "./containers/PostsList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Eyepax JavaScript Hackathon</h1>
+      <CustomLayout>
+        <PostList />
+        <h1>Eyepax JavaScript Hackathon</h1>
+      </CustomLayout>
     </div>
   );
 }
